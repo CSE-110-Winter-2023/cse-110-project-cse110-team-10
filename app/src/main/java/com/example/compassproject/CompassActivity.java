864923ba10ae2603d01c1431e1bf264c0dd9cbb5
Ashwin Tyagi <ashwin.k.tyagi@gmail.com>
2023-02-11 14:46:44 -0800

    sets NESW radii dynamically based off of compass_face view

--- app/src/main/java/com/example/compassproject/CompassActivity.java
+++ app/src/main/java/com/example/compassproject/CompassActivity.java
@@ -2,12 +2,14 @@ package com.example.compassproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CompassActivity extends AppCompatActivity {
 
@@ -20,6 +22,37 @@ public class CompassActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
+        final ImageView compass = (ImageView) findViewById(R.id.compass_face);
+        ViewTreeObserver observer = compass.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                TextView north = (TextView) findViewById(R.id.compass_N);
+                TextView east = (TextView) findViewById(R.id.compass_E);
+                TextView south = (TextView) findViewById(R.id.compass_S);
+                TextView west = (TextView) findViewById(R.id.compass_W);
+
+                int rad = compass.getHeight() / 2;
+
+                ConstraintLayout.LayoutParams north_lp = (ConstraintLayout.LayoutParams) north.getLayoutParams();
+                north_lp.circleRadius = rad;
+                north.setLayoutParams(north_lp);
+
+                ConstraintLayout.LayoutParams east_lp = (ConstraintLayout.LayoutParams) east.getLayoutParams();
+                east_lp.circleRadius = rad;
+                east.setLayoutParams(east_lp);
+
+                ConstraintLayout.LayoutParams south_lp = (ConstraintLayout.LayoutParams) south.getLayoutParams();
+                south_lp.circleRadius = rad;
+                south.setLayoutParams(south_lp);
+
+                ConstraintLayout.LayoutParams west_lp = (ConstraintLayout.LayoutParams) west.getLayoutParams();
+                west_lp.circleRadius = rad;
+                west.setLayoutParams(west_lp);
+
+                compass.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
     }
 
     public void onAddLocationClicked(View view) {
