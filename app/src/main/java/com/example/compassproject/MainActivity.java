c1c95f1381e7243875847acd13cb7327709b6f7c
AnthonyZWang <anthonyzhiyaowang@gmail.com>
2023-02-11 10:12:14 -0800

    user location

--- app/src/main/java/com/example/compassproject/MainActivity.java
+++ app/src/main/java/com/example/compassproject/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.compassproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LiveData;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LocationService locationService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //request permission from user at runtime to use location
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+
+        locationService = LocationService.singleton(this);
+        LiveData<Pair<Double,Double>> loc = locationService.getLocation();
+
+        loc.observe(this, location-> {
+            System.out.println(location.first);
+            System.out.println(location.second);
+        });
+
+
     }
 
     public void onOpenCompassClick(View view) {
