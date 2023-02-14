f594702724f3d3466fbbe7827445ada56f7222ad
Ashwin Tyagi <ashwin.k.tyagi@gmail.com>
2023-02-14 15:03:33 -0800

    set up rotation thread to update in the background

--- app/src/main/java/com/example/compassproject/CompassActivity.java
+++ app/src/main/java/com/example/compassproject/CompassActivity.java
@@ -12,9 +12,17 @@ import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class CompassActivity extends AppCompatActivity {
     static int radius;
     SavedLocations savedLocations;
+    private ExecutorService backgroundThreadExecutor = Executors.newSingleThreadExecutor();
+    private Future<Void> future;
+
 
     /*
      * TODO: Update locations and angles for compass_N, compass_E, compass_S, compass_W
@@ -81,8 +89,50 @@ public class CompassActivity extends AppCompatActivity {
     }
 
     public void onAddLocationClicked(View view) {
+        this.future.cancel(true);
         Intent intent = new Intent(this, NewLocationActivity.class);
         startActivity(intent);
     }
 
+    private void rotateThreadHandler() {
+        this.future = backgroundThreadExecutor.submit(() -> {
+            int rotationDegrees = 0;
+            do {
+                //TODO: find rotation constant
+                final int rotDegreesCopy = rotationDegrees;
+
+                //update angles for N,E,S,W letters
+                TextView north = (TextView) findViewById(R.id.compass_N);
+                TextView east = (TextView) findViewById(R.id.compass_E);
+                TextView south = (TextView) findViewById(R.id.compass_S);
+                TextView west = (TextView) findViewById(R.id.compass_W);
+
+                ConstraintLayout.LayoutParams north_lp = (ConstraintLayout.LayoutParams) north.getLayoutParams();
+                north_lp.circleAngle = rotationDegrees;
+
+                ConstraintLayout.LayoutParams east_lp = (ConstraintLayout.LayoutParams) east.getLayoutParams();
+                east_lp.circleAngle = 90 + rotationDegrees;
+
+                ConstraintLayout.LayoutParams south_lp = (ConstraintLayout.LayoutParams) south.getLayoutParams();
+                south_lp.circleAngle = 180 + rotationDegrees;
+
+                ConstraintLayout.LayoutParams west_lp = (ConstraintLayout.LayoutParams) west.getLayoutParams();
+                west_lp.circleAngle =  270 + rotationDegrees;
+
+                /*
+                 * TODO: do calculations for each locations
+                 * Remember:  view_deg = location_degrees + rotation_degrees
+                 */
+
+                runOnUiThread(() -> {
+                    north.setLayoutParams(north_lp);
+                    east.setLayoutParams(east_lp);
+                    south.setLayoutParams(south_lp);
+                    west.setLayoutParams(west_lp);
+                });
+
+            } while (true);
+        });
+
+    }
 }
