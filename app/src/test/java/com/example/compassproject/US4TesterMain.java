17f6ce183c7687653c1cca7e50700dae91fe556b
vqnguyenSD <vqnguyen@ucsd.edu>
2023-02-19 09:46:54 -0800

    US4-Fixing Story Test

--- app/src/test/java/com/example/compassproject/US4TesterMain.java
+++ app/src/test/java/com/example/compassproject/US4TesterMain.java
@@ -1,6 +1,6 @@
 package com.example.compassproject;
+
 import static org.junit.Assert.assertEquals;
-import static org.robolectric.Shadows.shadowOf;
 
 import android.Manifest;
 import android.app.Application;
@@ -35,6 +35,8 @@ public class US4TesterMain {
     public void testRotate180(){
         float degreeN = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, 10.0, 0.0);
         float degreeE = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, 0, 10.0);
+        float degreeS = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, -10.0, 0);
+        float degreeW = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, 0.0, -10.0);
         ActivityScenario<CompassActivity> scenario = ActivityScenario.launch(CompassActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
@@ -50,9 +52,9 @@ public class US4TesterMain {
             ConstraintLayout.LayoutParams loc_lpN = (ConstraintLayout.LayoutParams) locN.getLayoutParams();
             ConstraintLayout.LayoutParams loc_lpE = (ConstraintLayout.LayoutParams) locE.getLayoutParams();
             assertEquals(1, loc_lpN.circleRadius);
-            assertEquals(degreeN, loc_lpN.circleAngle, 0);
+            assertEquals(degreeS,DegreeCalculator.rotatingToPhoneOrientation(loc_lpN.circleAngle, activity.getCurrOrientation()) , 0);
             assertEquals(1, loc_lpE.circleRadius);
-            assertEquals(degreeE, loc_lpE.circleAngle, 0);
+            assertEquals(degreeW,DegreeCalculator.rotatingToPhoneOrientation(loc_lpE.circleAngle, activity.getCurrOrientation()) , 0);
         });
     }
 
@@ -61,6 +63,7 @@ public class US4TesterMain {
         float degreeN = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, 10.0, 0.0);
         float degreeE = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, 0, 10.0);
         float degreeS = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, -10.0, 0);
+        float degreeW = DegreeCalculator.degreeBetweenCoordinates(0.0, 0.0, 0.0, -10.0);
 
         ActivityScenario<CompassActivity> scenario = ActivityScenario.launch(CompassActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
@@ -73,17 +76,19 @@ public class US4TesterMain {
             View locN = DisplayHelper.displaySingleLocation((CompassActivity)activity, 1, 1, degreeN);
             View locE = DisplayHelper.displaySingleLocation((CompassActivity)activity, 1, 1, degreeE);
             View locS = DisplayHelper.displaySingleLocation((CompassActivity)activity, 1, 1, degreeS);
+
             //change phone's orientation
             mockOrientation.setValue((float) Math.PI/2);
             ConstraintLayout.LayoutParams loc_lpN = (ConstraintLayout.LayoutParams) locN.getLayoutParams();
             ConstraintLayout.LayoutParams loc_lpE = (ConstraintLayout.LayoutParams) locE.getLayoutParams();
             ConstraintLayout.LayoutParams loc_lpS = (ConstraintLayout.LayoutParams) locS.getLayoutParams();
+
             assertEquals(1, loc_lpN.circleRadius);
-            assertEquals(degreeN, loc_lpN.circleAngle, 0);
+            assertEquals(degreeW, DegreeCalculator.rotatingToPhoneOrientation(loc_lpN.circleAngle, activity.getCurrOrientation()), 0);
             assertEquals(1, loc_lpE.circleRadius);
-            assertEquals(degreeE, loc_lpE.circleAngle, 0);
+            assertEquals(degreeN, DegreeCalculator.rotatingToPhoneOrientation(loc_lpE.circleAngle, activity.getCurrOrientation()), 0);
             assertEquals(1, loc_lpS.circleRadius);
-            assertEquals(degreeS, loc_lpS.circleAngle, 0);
+            assertEquals(degreeE,DegreeCalculator.rotatingToPhoneOrientation(loc_lpS.circleAngle, activity.getCurrOrientation()) , 0);
         });
     }
 
