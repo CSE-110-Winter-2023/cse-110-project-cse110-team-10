de1ede9e8da91d6e313272b1f2f78886a085fb2f
Arnav Ghai <arnavghai@arnavs-air.lan>
2023-03-05 18:40:34 -0800

    Added DB/Server Code

---
+++ app/src/main/java/com/example/compassproject/model/LocationPut.java
@@ -0,0 +1,43 @@
+package com.example.compassproject.model;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+public class LocationPut {
+
+    /** The private code of the person. */
+    @SerializedName("private_code")
+    public String private_code;
+
+    @SerializedName("label")
+    @NonNull
+    public String label;
+
+    @SerializedName("latitude")
+    @NonNull
+    public double latitude;
+
+    @SerializedName("longitude")
+    @NonNull
+    public double longitude;
+
+    public LocationPut(Location loc){
+        this.private_code = loc.private_code;
+        this.label = loc.label;
+        this.latitude = loc.latitude;
+        this.longitude = loc.longitude;
+    }
+
+    public static LocationPut fromJSON(String json) {
+        return new Gson().fromJson(json, LocationPut.class);
+    }
+
+    public String toJSON() {
+        return new Gson().toJson(this);
+    }
+
+
+
+}
