d765c79f5b547e214c8634913dee2565070710f4
vqnguyenSD <vqnguyen@ucsd.edu>
2023-03-05 21:42:03 -0800

    US4-1, 6
    Refactoring the CompassActivity, and added a ViewModel folder.

--- app/src/main/java/com/example/compassproject/CompassActivity2.java
+++ app/src/main/java/com/example/compassproject/CompassActivity2.java
@@ -2,19 +2,24 @@
 
 package com.example.compassproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.lifecycle.LiveData;
-
 import android.os.Bundle;
 import android.util.Pair;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.compassproject.ViewModel.CompassViewModel;
 import com.example.compassproject.model.Location;
 import com.example.compassproject.model.LocationRepository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -26,6 +31,13 @@ public class CompassActivity2 extends AppCompatActivity {
     private int radius;
     private ImageView compass;
 
+    CompassViewModel viewModel;
+
+    ArrayList<LiveData<Location>> locationArray;
+
+    ConstraintLayout.LayoutParams north_lp, south_lp, east_lp, west_lp;
+    TextView north, south, east, west;
+
     LocationRepository locRepo;
 
     @Override
@@ -33,10 +45,10 @@ public class CompassActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
-        locRepo = LocationRepository.
 
-                // Create location and orientation services
-                LocationService ls = LocationService.singleton(this);
+        viewModel = setupViewModel();
+        // Create location and orientation services
+        LocationService ls = LocationService.singleton(this);
         OrientationService os = OrientationService.singleton(this);
 
         // Make sure compass has been set up on UI
@@ -48,19 +60,31 @@ public class CompassActivity2 extends AppCompatActivity {
                 setupUI();
 
                 // Continuously update location data to local fields
-                ls.getLocation().observe(this, location -> {
+                ls.getLocation().observe(CompassActivity2.this, location -> {
                     updateCoordinates(location);
-                    updateFriendLocations();
+                    updateAllFriendLocations();
+
                 });
 
                 // Continuously update orientation data to local fields
-                os.getOrientation().observe(this, orientation -> {
+                os.getOrientation().observe(CompassActivity2.this, orientation -> {
                     updateOrientation(orientation);
+                    updateCardinalAxisLabels();
+                    updateAllFriendLocations();
                 });
             }
         });
     }
 
+    public void updateAllFriendLocations(){
+        // showing red dots which is saved user locations
+        for(int i = 0; i < locationArray.size(); i++){
+            LiveData<Location> currLocLive = locationArray.get(i);
+            Location currLoc = currLocLive.getValue();
+
+            updateFriendLocations(currLoc);
+        }
+    }
     // Update coordinates both locally and remotely
     private void updateCoordinates(Pair<Double, Double> location) {
         updateCoordinatesLocal(location);
@@ -73,7 +97,10 @@ public class CompassActivity2 extends AppCompatActivity {
     }
 
     private void updateCoordinatesRemote(Pair<Double, Double> location){
-        //TODO: implement
+        viewModel.updateCoordinatesRemote(location);
+    }
+    private CompassViewModel setupViewModel() {
+        return new ViewModelProvider(this).get(CompassViewModel.class);
     }
 
     private void setupUI() {
@@ -81,29 +108,42 @@ public class CompassActivity2 extends AppCompatActivity {
         setupFriendLocations();
     }
 
+    private void updateCardinalAxisLabels(){
+        north_lp.circleAngle = (float) -currOrientation;
+        east_lp.circleAngle = (float) (90 - currOrientation);
+        south_lp.circleAngle = (float) (180 - currOrientation);
+        west_lp.circleAngle = (float) (270 - currOrientation);
+
+        // Place cardinal direction labels in correct location
+        north.setLayoutParams(north_lp);
+        east.setLayoutParams(east_lp);
+        south.setLayoutParams(south_lp);
+        west.setLayoutParams(west_lp);
+    }
+
     private void setupCardinalAxisLabels(){
         // Access UI elements for cardinal directions
-        TextView north = (TextView) findViewById(R.id.compass_N);
-        TextView east = (TextView) findViewById(R.id.compass_E);
-        TextView south = (TextView) findViewById(R.id.compass_S);
-        TextView west = (TextView) findViewById(R.id.compass_W);
+         north = (TextView) findViewById(R.id.compass_N);
+         east = (TextView) findViewById(R.id.compass_E);
+         south = (TextView) findViewById(R.id.compass_S);
+         west = (TextView) findViewById(R.id.compass_W);
 
         setRadius();
 
         // Placing cardinal direction labels in correct initial location
-        ConstraintLayout.LayoutParams north_lp = (ConstraintLayout.LayoutParams) north.getLayoutParams();
+        north_lp = (ConstraintLayout.LayoutParams) north.getLayoutParams();
         north_lp.circleRadius = radius;
         north.setLayoutParams(north_lp);
 
-        ConstraintLayout.LayoutParams east_lp = (ConstraintLayout.LayoutParams) east.getLayoutParams();
+        east_lp = (ConstraintLayout.LayoutParams) east.getLayoutParams();
         east_lp.circleRadius = radius;
         east.setLayoutParams(east_lp);
 
-        ConstraintLayout.LayoutParams south_lp = (ConstraintLayout.LayoutParams) south.getLayoutParams();
+        south_lp = (ConstraintLayout.LayoutParams) south.getLayoutParams();
         south_lp.circleRadius = radius;
         south.setLayoutParams(south_lp);
 
-        ConstraintLayout.LayoutParams west_lp = (ConstraintLayout.LayoutParams) west.getLayoutParams();
+        west_lp = (ConstraintLayout.LayoutParams) west.getLayoutParams();
         west_lp.circleRadius = radius;
         west.setLayoutParams(west_lp);
     }
@@ -115,17 +155,20 @@ public class CompassActivity2 extends AppCompatActivity {
 
     private void setupFriendLocations(){
         // TODO: Use Friends Database to get list of UIDs
-        List<String> friendList = FriendRepo.getAllFriendUIDs();
+        List<String> friendList = viewModel.getAllFriendUIDs(); // Move to ViewModel
         int numFriends = friendList.size();
 
         // showing red dots which is saved user locations
         for(int i = 0; i < numFriends; i++){
-            LiveData<Location> currLocLive = LocationRepository.getSynced(friendList.get(i));
+            LiveData<Location> currLocLive = viewModel.getLiveLocation(friendList.get(i)); // Move to ViewModel
             Location currLoc = currLocLive.getValue();
 
             // Create circle in the given angle
             CircleView loc_view = DisplayHelper.displaySingleLocation(CompassActivity.this, 1, radius-64, getDegree(currLoc));
             locMap.put(currLoc.public_code, loc_view);
+
+            // caching background thread
+            locationArray.add(currLocLive);
 
             //Will be replaced with new code for handling CircleView vs Label
             loc_view.setIndex(i);
---
+++ app/src/main/java/com/example/compassproject/ViewModel/CompassViewModel.java
@@ -0,0 +1,45 @@
+package com.example.compassproject.ViewModel;
+
+
+import android.app.Application;
+import android.util.Pair;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.compassproject.model.Location;
+import com.example.compassproject.model.LocationDatabase;
+import com.example.compassproject.model.LocationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompassViewModel extends AndroidViewModel {
+    private LiveData<Location> location;
+    private final LocationRepository locRepo;
+
+
+    public CompassViewModel(@NonNull Application application) {
+        super(application);
+        var context = application.getApplicationContext();
+        var locDb = LocationDatabase.provide(context);
+        var locDao = locDb.getDao();
+        this.locRepo = new LocationRepository(locDao);
+        //TODO: Add friend's database
+    }
+    public void updateCoordinatesRemote(Pair<Double, Double> location){
+        //TODO: Use real public_code, private_code, and label
+        Location loc1 = new Location("kgupta", "Team10TestInput", "Kanishk", location.first, location.second, true, "2023-02-18T12:00:00Z", "2023-02-18T18:30:00Z");
+        locRepo.upsertRemote(loc1);
+    }
+    public List<String> getAllFriendUIDs(){
+        //TODO: GETTING OUR FRIEND's UID FROM DATABASE
+        return new ArrayList<String>();
+    }
+
+    public LiveData<Location> getLiveLocation(String public_code){
+        return locRepo.getSynced(public_code);
+    }
+
+}
