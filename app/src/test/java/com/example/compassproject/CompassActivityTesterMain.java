e9c11d888a24c73a0c62e23ff3556748ef2e92bf
Ashwin Tyagi <ashwin.k.tyagi@gmail.com>
2023-03-08 13:49:30 -0800

    moved tests from DisplayHelperTester to CompassActivityTesterMain where it should have been in the 1st place

--- app/src/test/java/com/example/compassproject/CompassActivityTesterMain.java
+++ app/src/test/java/com/example/compassproject/CompassActivityTesterMain.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import android.Manifest;
 import android.app.Application;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.Lifecycle;
@@ -45,4 +46,36 @@ public class CompassActivityTesterMain {
     }
 
  */
+
+    @Before
+    public void setup() {
+        Application application = ApplicationProvider.getApplicationContext();
+        ShadowApplication app = Shadows.shadowOf(application);
+        app.grantPermissions(Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    @Test
+    public void testCircleViewIfFar() {
+        ActivityScenario<CompassActivity> scenario = ActivityScenario.launch(CompassActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.onActivity(activity -> {
+            View view = DisplayHelper.displaySingleLocation(activity, 1,400, 0,100, 40, "test" );
+
+            assertEquals(true, view instanceof CircleView);
+
+        });
+    }
+
+    public void testTextViewIfClose() {
+        ActivityScenario<CompassActivity> scenario = ActivityScenario.launch(CompassActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.onActivity(activity -> {
+            View view = DisplayHelper.displaySingleLocation(activity, 1,400, 0,20, 40, "test" );
+
+            assertEquals(true, view instanceof TextView);
+
+        });
+    }
 }
